dbe37fa922bd7f5db35453d4351ee2440b938061
Jerry <sa517301@mail.ustc.edu.cn>
2022-09-09 03:09:36 +0000

    is graph bipartite

---
+++ 09.Graph/3.bipartite_graph/785.is_graph_bipartite.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        """1.traversing the graph.
+           2.coloring while traversing the graph.
+           3.return True if the entire graph is corlored, 
+             return False if the neighbor nodes have the same color and terminate immediately.
+        """
+        n = len(graph)
+        self.visited = [False] * n
+        self.color = [False] * n
+        self.is_bipartite = True
+        # traversing all subgraphs because subgraphs are not necessarily all connected.
+        for cur_node in range(0, n):
+            if self.visited[cur_node] == False:
+                self.traversal(graph, cur_node)
+        return self.is_bipartite
+    
+    def traversal(self, graph, cur_node):
+        if not self.is_bipartite:
+            return
+        self.visited[cur_node] = True
+        for neighbor in graph[cur_node]:
+            if self.visited[neighbor] == False:
+                self.color[neighbor] = not self.color[cur_node]
+                self.traversal(graph, neighbor)
+            else:
+                if self.color[neighbor] == self.color[cur_node]:
+                    self.is_bipartite = False
+                    return
+        
